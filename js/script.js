374471a837ec723064170886f6397c10053315e2
Smoke-the-blade <alejandroturiot@gmail.com>
2025-03-25 17:29:22 -0300

    armando la pagina 1.564.01

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*document.addEventListener('DOMContentLoaded', () => {
+    const toggleButtons = document.querySelectorAll('.toggle-btn');
+  
+    toggleButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const nextElement = button.nextElementSibling;
+        if (nextElement && nextElement.tagName === 'UL' || nextElement.classList.contains('image-container')) {
+          nextElement.style.display = nextElement.style.display === 'none' ? 'block' : 'none';
+        }
+      });
+    });
+  }); */
+
+
+ document.addEventListener("DOMContentLoaded", () => {
+  console.log("¡JS cargado correctamente!");
+ });
