2f59b9109c81132a089ff4687eabe8a74c161f5a
Smoke-the-blade <alejandroturiot@gmail.com>
2025-03-25 18:11:29 -0300

    a ver si se ven las imagenes

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@
         if (nextElement && nextElement.tagName === 'UL' || nextElement.classList.contains('image-container')) {
           nextElement.style.display = nextElement.style.display === 'none' ? 'block' : 'none';
         }
-      });
     });
   }); */
 
@@ -15,3 +14,21 @@
  document.addEventListener("DOMContentLoaded", () => {
   console.log("¡JS cargado correctamente!");
  });
+
+ const imagen = document.getElementById("imagen-principal");
+    const boton = document.getElementById("boton-cambiar");
+
+    const carpetas = ["Aurelius Shadowcaster", "Darión", "Grimnar Khyber", "LuminusPiesligeros", "Morthos Tormento", "Mr. Maldito Cuervo", "Nus", "Sam el Lobo Malone", "Shokan Lograrn"];
+    const imagenesPorCarpeta = ["Aurelius.JPEG", "Dorian.JPEG", "gran biblioteca 2.JPEG", "gran biblioteca.JPEG", "Liondria", "Darion Riverblood", "Amuleto del Rayo vivo 1", "Armadura de Guardia Electrica", "Armadura de Infiltracion Electrica", "Grimm",];
+
+    const obtenerImagenAleatoria = () => {
+      const carpetaAleatoria = carpetas[Math.floor(Math.random() * carpetas.length)];
+      const imagenAleatoria = imagenesPorCarpeta[Math.floor(Math.random() * imagenesPorCarpeta.length)];
+      return `imagenes/${carpetaAleatoria}/${imagenAleatoria}`;
+    };
+
+    boton.addEventListener("click", () => {
+      const nuevaImagen = obtenerImagenAleatoria();
+      imagen.src = nuevaImagen;
+      console.log("Imagen cambiada a: " + nuevaImagen);
+  });
